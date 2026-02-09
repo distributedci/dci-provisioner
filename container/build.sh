67cb05d732addafc3f97c5250306b5e8532b6ce0
Michael Burke <miburke@redhat.com>
2026-02-09 13:09:44 -0500

    Updating for quay_expiration env var

--- container/build.sh
+++ container/build.sh
@@ -13,8 +13,8 @@ PROVISIONER_IMAGE="${REGISTRY_BASE}/dci-provisioner:${VERSION}"
 LAB_IMAGE="${REGISTRY_BASE}/dci-lab:${VERSION}"
 
 LABEL_OPTS=""
-if [[ "$VERSION" == git* ]]; then
-    LABEL_OPTS="--label quay.expires-after=2w"
+if [[ -n "$QUAY_EXPIRATION" ]]; then
+     LABEL_OPTS="--label quay.expires-after=$QUAY_EXPIRATION"
 fi
 
 podman build $LABEL_OPTS -f Containerfiles/Containerfile-provisioner -t "$PROVISIONER_IMAGE" .
